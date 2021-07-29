e4338b6999ea2de3ee88b27e9eb35c3f8ae3ca6e
Kenneth Harris <kharris@mbp-kharris.theshelbygroup.com>
2021-07-29 16:21:53 -0500

    Slight changes to the support page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,13 @@ const Hero = () => {
   return (
     <>
       <div>
-        <div className='flex justify-center'>
+        <div className='flex'>
           <img src={proPic} alt="ProPic" />
-          <div className=''>
-            <h1>Kenneth Harris </h1>
-            <p> Web Developer </p>
-          </div>
+        </div>
+        <div>
+          <h1>Kenneth Harris </h1>
+          <p> Hello!  I see you have made it to my website.  There's not much to see yet, but best believe there will be way more content soon.
+            Very soon...</p>
         </div>
       </div>
 
--- src/pages/Support.js
+++ src/pages/Support.js
@@ -4,6 +4,8 @@ const Support = () => {
   return (
     <div>
       <h1 className="flex justify-center">COMING SOON</h1>
+      <a href="https://kineticaesthetic.net"> Podcast!</a>
+      <a href="">Stonerville</a>
     </div>
   )
 }
