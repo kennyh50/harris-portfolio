b2a871b4b5d831b92f4313d8bb5444d6df0282dd
Kenneth Harris <kharris@mbp-kharris.local>
2021-07-30 15:04:37 -0500

    Turned on dark mode

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,18 +5,17 @@ const Hero = () => {
   return (
     <>
       <div className='flex justify-center mt-4'>
+        <img src={proPic} alt="ProPic" className='rounded-full' />
 
-        <img src={proPic} alt="ProPic" className='rounded-lg' />
+        <p>I'm testing out the blockquote thing I just found out about.  Idk what it does, but I have high hopes.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nobis dolorum quos quibusdam doloremque! Hic esse illo cum. A nobis pariatur facilis aspernatur sapiente ad? Molestiae vero odit veniam quasi!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, doloribus! Amet expedita laboriosam quisquam enim quidem repellendus, modi fugit, porro cupiditate ut sapiente eos et quae laudantium quia ducimus ab.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ut cumque veritatis illo dignissimos voluptatem cupiditate repudiandae odio harum itaque odit nam accusamus maxime eaque, dolor ipsam eius natus ea.
+          Iure, nesciunt. Ad iusto laborum odit consequuntur iure vel qui quidem accusamus ab non temporibus vitae id, eius totam aliquid corrupti quaerat similique cumque rerum deleniti ipsam. Nesciunt, deleniti. Recusandae?
+          Libero recusandae voluptatum facilis blanditiis impedit dolores itaque officia saepe explicabo iure debitis modi mollitia, neque nemo, deserunt vero. Quaerat error suscipit beatae delectus a voluptatibus ut sequi commodi voluptatem!
+        </p>
 
-
-        <h1 className='font-black'>Kenneth Harris </h1>
-
-        <div>
-
-        </div>
       </div>
-
-
     </>
 
 
